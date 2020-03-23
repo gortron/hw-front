d48f3b6f052874b04c606aeb7985bb37b2e279df
gortron <gordon.lanza@gmail.com>
2020-03-23 11:15:19 -0700

    set up state and mounting with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import _ from "lodash";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const getStudents = async () => {
+      const response = await fetch(
+        "https://www.hatchways.io/api/assessment/students"
+      );
+      const data = await response.json();
+      setStudents(data.students);
+    };
+    getStudents();
+  }, []);
+
+  const renderStudents = () => {
+    if (students.length === 0) {
+      return <p>Loading...</p>;
+    } else {
+      return students.map(student => {
+        return (
+          <div>
+            <img src={student.pic} alt="student pic" />
+            <h3>{student.firstName}</h3>
+            <p>Email: {student.email}</p>
+            <p>Company: {student.company}</p>
+            <p>Skill: {student.skill}</p>
+            <p>
+              Grades:{" "}
+              {_.meanBy(student.grades, function(g) {
+                return parseInt(g);
+              })}
+            </p>
+          </div>
+        );
+      });
+    }
+  };
+
+  return <div className="App">{renderStudents()}</div>;
+};
 
 export default App;
